ab0f8108f5800ad0ac7c3f8864165eb3d8c26881
Jay Schauer <unmet-teacups-8g@icloud.com>
2025-09-09 12:30:50 -0400

    Make dp solution linear in memory

--- 2327.cpp
+++ 2327.cpp
@@ -7,45 +7,56 @@ using namespace std;
 class Solution {
  public:
   int peopleAwareOfSecret(int n, int delay, int forget) {
-    // Let dp[i][j] be the number of people who have known the secret for
-    // exactly j + 1 days, at day i.
-    vector<vector<int>> dp(n, vector(n, 0));
-    int modulo = 1000000000 + 7;
+    // p.second stores the number of people who know the secret for p.first days
+    queue<pair<int, int>> knowers;
 
-    // day 1:
-    dp[0][0] = 1;  // end of day 1, 1 person knows the secret
+    // End of day 1, 1 person knows the secret:
+    knowers.emplace(1, 1);
 
     // days 2-n:
     for (int i = 1; i < n; i++) {
-      // Copy people from yesterday who know the secret but haven't shared yet
-      for (int j = 0; j < delay - 1; j++) {
-        // People at day i who have known for j+2 days are
-        // the people at day i-1 who have known for j+1 days;
-        dp[i][j + 1] = dp[i - 1][j];
-      }
-      //
-      // Copy people from yesterday who know the secret, haven't forgotton, and
-      // are sharing it
       int learners = 0;
-      for (int j = delay - 1; j < forget - 1; j++) {
-        // Why delay-1? Can share today if end of day yesterday was your
-        // delay'th day WHy forget-1? Can share today if end of day yesterday
-        // you weren't at forget yet
-        learners = (learners + dp[i - 1][j]) % modulo;
-        dp[i][j + 1] = dp[i - 1][j];
+
+      while (true) {
+        const pair<int, int>& front = knowers.front();
+
+        // Skip people who forget today
+        if (front.first >= forget) {
+          knowers.pop();
+          continue;
+        }
+
+        // Copy people who haven't forgot forward
+        knowers.emplace(front.first + 1, front.second);
+
+        // if they've reached delay, add the people they share with to learners
+        if (front.first >= delay) {
+          learners = (learners + front.second) % modulo;
+        }
+
+        // Base case: learners from yesterday. They
+        if (front.first == 1) {
+          knowers.pop();
+          break;
+        }
+
+        knowers.pop();
       }
-      //
-      // set the people today who have learned
-      dp[i][0] = learners;
+
+      knowers.emplace(1, learners);
     }
 
     // return the number of people who know the secret at the end of day n
     int total = 0;
-    for (int j = 0; j < n; j++) {
-      total = (total + dp[n - 1][j]) % modulo;
+    while (!knowers.empty()) {
+      total = (total + knowers.front().second) % modulo;
+      knowers.pop();
     }
     return total;
   }
+
+ private:
+  static constexpr int modulo = 1000000000 + 7;
 };
 
 int main() {
