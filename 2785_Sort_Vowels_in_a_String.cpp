f3107847e7ec73d4b93bef8ecc388236d2b43436
Jay Schauer <unmet-teacups-8g@icloud.com>
2025-09-11 18:23:55 -0400

    Solve 2785

--- 2785_Sort_Vowels_in_a_String.cpp
+++ 2785_Sort_Vowels_in_a_String.cpp
@@ -8,7 +8,58 @@ using namespace std;
  */
 class Solution {
  public:
-  string sortVowels(string s) { return ""; }
+  string sortVowels(string s) {
+    // Checking vector should be fast enough, no need for hashmap
+    vector<pair<char, int>> vowel_count = {
+        {'A', 0}, {'E', 0}, {'I', 0}, {'O', 0}, {'U', 0},
+        {'a', 0}, {'e', 0}, {'i', 0}, {'o', 0}, {'u', 0}};
+
+    // Count the vowels in the string:
+    for (char c : s) {
+      auto it = find_if(vowel_count.begin(), vowel_count.end(),
+                        [c](const pair<char, int>& p) { return p.first == c; });
+      if (it != vowel_count.end()) {
+        it->second++;
+      }
+    }
+
+    // Remove from vowel_count vowels that aren't in the string:
+    auto vowel_end =
+        remove_if(vowel_count.begin(), vowel_count.end(),
+                  [](const pair<char, int>& p) { return p.second == 0; });
+
+    if (vowel_end == vowel_count.begin()) {
+      return s;
+    }
+
+    auto vowel_replace = vowel_count.begin();
+
+    // Rearrange the vowels:
+    for (int i = 0; i < s.size(); i++) {
+      char c = s[i];
+      auto it = find_if(vowel_count.begin(), vowel_end,
+                        [c](const pair<char, int>& p) { return p.first == c; });
+
+      if (it == vowel_end) {
+        continue;
+      }
+
+      // if vowel is out of place, update it
+      if (c != vowel_replace->first) {
+        s[i] = vowel_replace->first;
+      }
+      vowel_replace->second--;
+      if (vowel_replace->second == 0) {
+        vowel_replace++;
+      }
+
+      if (vowel_replace == vowel_end) {
+        break;  // done updating vowels
+      }
+    }
+
+    return s;
+  }
 };
 
 int main() {
