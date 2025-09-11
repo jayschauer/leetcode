09f1312c40c84189b78067d4ced55c38538392c0
Jay Schauer <unmet-teacups-8g@icloud.com>
2025-09-11 17:25:15 -0400

    Create scaffolding for 2785

---
+++ 2785_Sort_Vowels_in_a_String.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * 2785. Sort Vowels in a String
+ *
+ * https://leetcode.com/problems/sort-vowels-in-a-string/description
+ */
+class Solution {
+ public:
+  string sortVowels(string s) { return ""; }
+};
+
+int main() {
+  Solution sol;
+
+  cout << sol.sortVowels("lEetcOde") << endl;
+  cout << sol.sortVowels("lYmpH") << endl;
+
+  return 0;
+}
