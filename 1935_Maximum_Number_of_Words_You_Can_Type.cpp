21ee970dcef5968683a9f7d8a28bfbb03ed88329
Jay Schauer <unmet-teacups-8g@icloud.com>
2025-09-15 10:14:03 -0400

    Solve 1935

--- 1935_Maximum_Number_of_Words_You_Can_Type.cpp
+++ 1935_Maximum_Number_of_Words_You_Can_Type.cpp
@@ -8,20 +8,61 @@ using namespace std;
  */
 class Solution {
  public:
-  int canBeTypedWords(string text, string brokenLetters) { return 0; }
+  int canBeTypedWords(string text, string brokenLetters) {
+    if (brokenLetters.size() == 0) {
+      return count(text.begin(), text.end(), ' ') + 1;
+    }
+
+    vector<bool> is_broken_letter(26, false);
+    for (char c : brokenLetters) {
+      is_broken_letter[c - 'a'] = true;
+    }
+
+    int count = 0;
+    bool canType = true;
+
+    for (char c : text) {
+      // We've finished a word - increment count if we can type it
+      if (c == ' ') {
+        // Note: this check is safe because there's no leading spaces
+        if (canType) {
+          count++;
+        }
+        canType = true;
+        continue;
+      }
+
+      // We've already found we can't type this word - skip to the next one
+      if (!canType) {
+        continue;
+      }
+
+      if (is_broken_letter[c - 'a']) {
+        canType = false;
+      }
+    }
+
+    if (canType) {
+      // Note: no trailing spaces, so if we get to here then we've finished
+      // checking the list with a word we can type
+      count++;
+    }
+
+    return count;
+  }
 };
 
 int main() {
   Solution sol;
 
   string text = "hello world", brokenLetters = "ad";
-  cout << sol.canBeTypedWords(text, brokenLetters) << endl;
+  cout << sol.canBeTypedWords(text, brokenLetters) << endl;  // 1
 
   text = "leet code", brokenLetters = "lt";
-  cout << sol.canBeTypedWords(text, brokenLetters) << endl;
+  cout << sol.canBeTypedWords(text, brokenLetters) << endl;  // 1
 
   text = "leet code", brokenLetters = "e";
-  cout << sol.canBeTypedWords(text, brokenLetters) << endl;
+  cout << sol.canBeTypedWords(text, brokenLetters) << endl;  // 0
 
   return 0;
 }
