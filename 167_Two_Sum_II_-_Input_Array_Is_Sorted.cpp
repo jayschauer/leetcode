844e95dc5c7d740b32cd715c5fa5134d6263365c
Jay Schauer <unmet-teacups-8g@icloud.com>
2025-09-15 10:25:42 -0400

    Solve 167

--- 167_Two_Sum_II_-_Input_Array_Is_Sorted.cpp
+++ 167_Two_Sum_II_-_Input_Array_Is_Sorted.cpp
@@ -9,7 +9,24 @@ using namespace std;
 
 class Solution {
  public:
-  vector<int> twoSum(vector<int>& numbers, int target) { return {1, 2}; }
+  vector<int> twoSum(vector<int>& numbers, int target) {
+    if (numbers.size() == 2) {
+      return {1, 2};
+    }
+
+    int a = 0;
+    int b = numbers.size() - 1;
+
+    while ((numbers[a] + numbers[b]) != target) {
+      if ((numbers[a] + numbers[b]) > target) {
+        b--;
+      } else {
+        a++;
+      }
+    }
+
+    return {a + 1, b + 1};
+  }
 };
 
 int main() {
@@ -18,17 +35,17 @@ int main() {
   vector<int> numbers{2, 7, 11, 15};
   int target = 9;
   vector<int> result = sol.twoSum(numbers, target);
-  cout << result[0] << " " << result[1] << endl;
+  cout << result[0] << " " << result[1] << endl;  // 1, 2
 
   numbers = {2, 3, 4};
   target = 6;
   result = sol.twoSum(numbers, target);
-  cout << result[0] << " " << result[1] << endl;
+  cout << result[0] << " " << result[1] << endl;  // 1, 3
 
   numbers = {-1, 0};
   target = -1;
   result = sol.twoSum(numbers, target);
-  cout << result[0] << " " << result[1] << endl;
+  cout << result[0] << " " << result[1] << endl;  // 1, 2
 
   return 0;
 }
