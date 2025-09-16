89b0b2e856398efbdd38af240c0f6466adc30d4b
Jay Schauer <unmet-teacups-8g@icloud.com>
2025-09-16 15:07:45 -0400

    Solve 3 sum with sorting

--- 15_3Sum.cpp
+++ 15_3Sum.cpp
@@ -9,8 +9,61 @@ using namespace std;
 class Solution {
  public:
   vector<vector<int>> threeSum(vector<int>& nums) {
-    // todo
-    return {};
+    // Naive solution: just compare everything, O(n^3)
+
+    // Can repeat what we did for 2-some: sort the array, then make the target
+    // each element of the array. That gives us a O(n^2) algorithm and O(1)
+    // space (not counting the input).
+
+    // Either O(1) or O(n) space depending on the algorithm.
+    sort(nums.begin(), nums.end());
+
+    vector<vector<int>> result;
+
+    for (int i = 0; i < nums.size(); i++) {
+      if (nums[i] > 0) {
+        // This is just an optimization;
+        // we know everything after i is greater than it, so it won't make our
+        // sum smaller to be zero.
+        break;
+      }
+
+      if (i > 0 && nums[i] == nums[i - 1]) {
+        // Want to avoid duplicate triplets.
+        // So we skip an element equal to one we've already checked.
+        // Only need to compare adjacent elements because we've sorted the
+        // numbers.
+        continue;
+      }
+      int j = i + 1;
+      int k = nums.size() - 1;
+
+      while (j < k) {
+        int sum = nums[i] + nums[j] + nums[k];
+        if (sum > 0) {
+          k--;  // need to make sum smaller
+        } else if (sum < 0) {
+          j++;  // need to make sum bigger
+        } else {
+          result.push_back({nums[i], nums[j], nums[k]});
+          // Increment j to try to find a new triplet.
+          j++;
+          // Can decrement k as well:
+          // If it still summed to zero with j++, that means j is the same and
+          // we don't have a distinct triplet.
+          k--;
+
+          while (j < k && (nums[j] == nums[j - 1])) {
+            // Again, we want to avoid duplicate triplets.
+            // So we skip over "j"s that we've already checked.
+            // Once we find a new j, that guarantees k will also be different.
+            j++;
+          }
+        }
+      }
+    }
+
+    return result;
   }
 };
 
