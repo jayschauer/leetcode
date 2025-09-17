8ddf70339e62b8ae98faccf22dbb0dfbd60c471d
Jay Schauer <unmet-teacups-8g@icloud.com>
2025-09-17 18:20:14 -0400

    Solve 125

---
+++ 125_Valid_Palindrome.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * 125. Valid Palindrome
+ *
+ * https://leetcode.com/problems/valid-palindrome/description/
+ */
+class Solution {
+ public:
+  bool isPalindrome(string s) {
+    int l = 0;
+    int r = s.length() - 1;
+    while (l < r) {
+      while (!isalnum(s[l]) && l < r) {
+        l++;
+      }
+      while (!isalnum(s[r]) && l < r) {
+        r--;
+      }
+      if (toupper(s[l]) != toupper(s[r])) {
+        return false;
+      }
+      l++;
+      r--;
+    }
+    return true;
+  }
+};
