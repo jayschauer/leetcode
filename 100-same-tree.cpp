e369f81c350a7ce65d08aa6d3b6b1378e40e168c
Jay Schauer <unmet-teacups-8g@icloud.com>
2025-12-24 22:52:42 -0600

    Solve 100

---
+++ 100-same-tree.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// https://leetcode.com/problems/same-tree/
+
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+ public:
+  bool isSameTree(TreeNode* p, TreeNode* q) {
+    if (!p && !q) {
+      return true;
+    }
+
+    if (p && !q || q && !p) {
+      return false;
+    }
+
+    if (p->val != q->val) {
+      return false;
+    }
+
+    if (!isSameTree(p->left, q->left)) {
+      return false;
+    }
+
+    if (!isSameTree(p->right, q->right)) {
+      return false;
+    }
+
+    return true;
+  }
+};
+
+int main() {
+  Solution sol;
+
+  cout << sol.isSameTree(nullptr, nullptr) << endl;
+
+  return 0;
+}
