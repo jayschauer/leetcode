29d84d71a0174b75621370199b41f6bc7a8e9088
Jay Schauer <unmet-teacups-8g@icloud.com>
2025-12-26 22:41:55 -0600

    Simpler solution for 235

--- 235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -16,49 +16,12 @@ struct TreeNode {
 class Solution {
  public:
   TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-    // DFS for p and q.
-    // Track the highest level where p and q are found.
-
-    // Given that node values are unique, we might be able to take advantage
-    // of serialization?
-    TreeNode* result = nullptr;
-    lowestCommonAncestorInner(root, p, q, result);
-    return result;
-  }
-
- private:
-  pair<bool, bool> lowestCommonAncestorInner(TreeNode* root, TreeNode* p,
-                                             TreeNode* q, TreeNode*& result) {
-    if (root == nullptr) {
-      return {false, false};
-    }
-
-    auto foundPQLeft = lowestCommonAncestorInner(root->left, p, q, result);
-    if (result) {
-      // p and q have a common ancestor down the left subtree
-      return {true, true};
-    }
-    auto foundPQRight = lowestCommonAncestorInner(root->right, p, q, result);
-    if (result) {
-      // p and q have a common ancestor down the right subtree
-      return {true, true};
-    }
-
-    pair<bool, bool> foundPQ{foundPQLeft.first || foundPQRight.first,
-                             foundPQLeft.second || foundPQRight.second};
-
-    if (root == p) {
-      foundPQ.first = true;
-    } else if (root == q) {
-      foundPQ.second = true;
-    }
-
-    if (foundPQ.first && foundPQ.second) {
-      result = root;
-      return {true, true};
-    }
-
-    return foundPQ;
+    if (!root || !p || !q) return nullptr;
+    if (p->val < root->val && q->val < root->val)
+      return lowestCommonAncestor(root->left, p, q);
+    if (p->val > root->val && q->val > root->val)
+      return lowestCommonAncestor(root->right, p, q);
+    return root;
   }
 };
 
